0b669d48495413834b077839709628dcec8bd59f
David Mehmke <davidmehmke@gmail.com>
2026-06-12 09:58:48 +0200

    Remove Market Stand game from portal and routing

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -3,7 +3,6 @@ import GamePortal from './pages/GamePortal'
 import CypherGame from './pages/CypherGame'
 import WerBinIchGame from './pages/WerBinIchGame'
 import WavvelengthGame from './pages/WavvelengthGame'
-import MarketStandGame from './pages/MarketStandGame'
 
 export default function Root() {
   return (
@@ -14,7 +13,6 @@ export default function Root() {
         <Route path="/werbinich/*" element={<WerBinIchGame />} />
         <Route path="/wavelength/*" element={<WavvelengthGame />} />
         <Route path="/wavvelength/*" element={<WavvelengthGame />} />
-        <Route path="/marketstand" element={<MarketStandGame />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
